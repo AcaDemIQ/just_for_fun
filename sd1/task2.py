f3a7e8fa8e23767a7762d89627036d90b511b259
Aleksey M. Mukhin <mukhin@bionet.nsc.ru>
2026-09-04 22:13:40 +0700

    task2 is finished

---
+++ sd1/task2.py
@@ -0,0 +1,21 @@
+import pytest
+
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, inc):
+        self.balance += inc
+
+    def withdraw(self, dec):
+        self.balance -= dec
+
+    def getBalance(self):
+        return self.balance
+
+
+def test_bank_account():
+    account = BankAccount(10)
+    account.deposit(20)
+    account.withdraw(30)
+    assert 0 == account.getBalance()
