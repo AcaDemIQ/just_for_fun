b2df94a71edba59891a23790fca51a6409b8c569
Aleksey M. Mukhin <mukhin@bionet.nsc.ru>
2026-09-07 23:30:35 +0700

    task8.py

--- sd1/task8.py
+++ sd1/task8.py
@@ -5,8 +5,8 @@ from zoneinfo import ZoneInfo
 DT_FORMAT="%Y-%m-%d %H:%M:%S" # format as constant
 
 if __name__ == "__main__":
-    date_str = "2024-05-13 14:30:00" # input, from task template 
-    dt = datetime.strptime(date_str, DT_FORMAT) # standart Python, nevermind
+    date_str = "2024-05-13T14:30:00" # input, from task template 
+    dt = datetime.fromisoformat(date_str)
     
     nsk_dt = dt.replace(tzinfo=ZoneInfo("Asia/Novosibirsk")) # DONE: added timezone with "current" time
     print(nsk_dt)
